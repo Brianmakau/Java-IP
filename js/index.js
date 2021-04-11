d880ec9daa80e8a6593d8e158eff87fa00332fa7
Brianmakau <brianmakau@student.moringaschool.com>
2021-04-11 22:00:05 +0300

    added  answer to submit button

--- js/index.js
+++ js/index.js
@@ -2,13 +2,18 @@ var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 //create a array denoting the different days of the week
 var day = ["0", "1", "2", "3", "4", "5", "6"];
-//create a function to extract date, month and year from entered dates of birth week
-function newDate = new Date(dob){
-  var month = newDate.getMonth()+1
-  var date = newDate.getdate()
-  var year = newDate.getFullYear()
-  console.log(month+"-"+date+"-"+year)
+//create a function to extract date, month and year from entered dates of birth
+function submitStaff(){
+  var date=new Date(form.birthday.value)
+  var day=date.getDay()
+  var result
+  var gender=form.gender.value
+  if(gender=="male"){
+    result=maleNames[day]
+  }
+  else if(gender=="female"){
+    result=femaleNames[day]
+  }
+  
 }
-//calculate day of the week using formula
-  //We first need to adjust the months to fit working using Zeller's Algorithhm
-  if (month===1||month===2)
+
