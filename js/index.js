89d1bb07b703baa62f3509bddedc44b27824d243
Brianmakau <brianmakau@student.moringaschool.com>
2021-04-11 22:24:35 +0300

    made the pahe neat

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
 //create a array denoting the different days of the week
 var day = ["0", "1", "2", "3", "4", "5", "6"];
+
 //create a function to extract date, month and year from entered dates of birth
 function submitStaff(){
   var date=new Date(form.birthday.value)
@@ -14,6 +16,6 @@ function submitStaff(){
   else if(gender=="female"){
     result=femaleNames[day]
   }
-  
+  document.getElementById("result").innerHTML=result
 }
 
