95a43d78b1a19cba7d00568f5f0e145e55e02ee1
Brianmakau <brianmakau@student.moringaschool.com>
2021-04-11 21:21:44 +0300

    added akan names to the files

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-var date = prompt("please enter you date of birth")
-
+var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+//create a array denoting the different days of the week
+var day = ["0", "1", "2", "3", "4", "5", "6"];
+//create a function to extract date, month and year from entered dates of birth week
+function newDate = new Date(dob){
+  var month = newDate.getMonth()+1
+  var date = newDate.getdate()
+  var year = newDate.getFullYear()
+  console.log(month+"-"+date+"-"+year)
+}
+//calculate day of the week using formula
+  //We first need to adjust the months to fit working using Zeller's Algorithhm
+  if (month===1||month===2)
